870cfbc109be77ef0683d22df417fa5a5f4abf55
haniiw <hokinethk@gmail.com>
2026-02-24 08:30:29 +0700

    wip: setup tailwind and setup views

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -5,13 +5,16 @@ class StudentController
 {
     public function index()
     {
-        echo '<h1>Daftar Siswa</h1>';
-        echo '<p>Menampilkan daftar siswa</p>';
+        require_once './app/views/students/index.php';
     }
 
     public function create()
     {
-        echo '<h1>Tambah Siswa</h1>';
-        echo '<p>Menampilkan form tambah siswa</p>';
+        require_once './app/views/students/create.php';
+    }
+
+    public function show(string $id)
+    {
+        require_once './app/views/students/show.php';
     }
 }
--- public/index.php
+++ public/index.php
@@ -4,5 +4,12 @@ use App\Core\Router;
 
 $router = new Router();
 
+
+// Register Routes 
+$router->add('GET', '/students', 'StudentController', 'index');
+$router->add('GET', '/students/create', 'StudentController', 'create');
+$router->add('GET', '/students/{id}', 'StudentController', 'show');
+
 $router->run();
+
 ?>
