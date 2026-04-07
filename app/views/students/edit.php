5af08519935e1771576951c75b09a96a2cbb6b16
haniiw <hokinethk@gmail.com>
2026-04-07 07:57:44 +0700

    feat: implement student detail and edit page

--- app/views/students/edit.php
+++ app/views/students/edit.php
@@ -11,7 +11,7 @@
     <header class="bg-blue-500 text-white">
         <div class="flex items-center justify-between container mx-auto p-4">
             <a href="/students" class="font-bold text-xl">Sistem Sekolah</a>
-            <a href="/students/create" class="bg-white text-blue-500 px-4 py-2 rounded-lg">Detail Siswa</a>
+            <a href="/students/create" class="bg-white text-blue-500 px-4 py-2 rounded-lg">Edit Siswa</a>
         </div>
     </header>
     <!-- Header End -->
@@ -21,34 +21,35 @@
         <div class="mt-8 space-y-4">
             <!-- Card Header Start-->
              <div class="bg-white shadow rounded-lg p-4">
-                <h1 class="text-xl font-bold">Detail Siswa</h1>
-                <p>Menambahkan detail siswa yang terdaftar</p>
+                <h1 class="text-xl font-bold">Edit Siswa</h1>
+                <p>Melakukan perubahan data siswa yang terdaftar</p>
              </div>
             <!-- Card Header End -->
 
             <!-- Card Content Start-->
              <div class="bg-white shadow rounded-lg">
-                <div class="p-4 grid grid-cols-2 gap-4">
+                <form action="" class="p-4 grid grid-cols-2 gap-4">
                     <div class="space-y-2">
                         <label class="block font-bold" for="name">Nama</label>
-                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="name" id="name" placeholder="Masukkan Nama" readonly>
+                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="name" id="name" placeholder="Masukkan Nama">
                     </div>
                     <div class="space-y-2">
                         <label class="block font-bold" for="nis">NIS</label>
-                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="nis" id="nis" placeholder="Masukkan NIS" readonly>
+                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="nis" id="nis" placeholder="Masukkan NIS">
                     </div>
                     <div class="space-y-2">
                         <label class="block font-bold" for="class">Kelas</label>
-                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="classs" id="class" placeholder="Masukkan Kelas" readonly>
+                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="classs" id="class" placeholder="Masukkan Kelas">
                     </div>
                     <div class="space-y-2">
                         <label class="block font-bold" for="phone_number">No Telepon</label>
-                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="phone_number" id="phone_number" placeholder="Masukkan No Telepon" readonly>
+                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="phone_number" id="phone_number" placeholder="Masukkan No Telepon">
                     </div>
                     <div class="flex justify-end gap-4 col-span-2">
                         <a href="/students" class="py-2 px-4 bg-gray-100 rounded-lg">Kembali</a>
+                        <button type="submit" class="py-2 px-4 bg-blue-500 rounded-lg text-white">Simpan</button>
                     </div>
-                </div>
+                </form>
              </div>
             <!-- Card Content End -->
 
