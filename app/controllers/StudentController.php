22a647f39f24990e1dd19e31fd36062a082bc981
haniiw <hokinethk@gmail.com>
2026-04-17 11:36:13 +0700

    feat: implement student detail page

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -25,7 +25,13 @@ class StudentController extends Controller
 
     public function show(string $id)
     {
-        $this->view('students.show');    
+        $id = intval($id);
+        $studentModel = new Student();
+        $student = $studentModel->getStudent($id);
+
+        $this->view('students.show', [
+            'student' => $student
+        ]);    
     }
 
     public function edit(string $id)
