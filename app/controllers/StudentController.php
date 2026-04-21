fd6e64afa4ee31614243cafc1fe6afd5246f7c87
haniiw <hokinethk@gmail.com>
2026-04-21 08:19:08 +0700

    feat: implement create page

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -38,4 +38,10 @@ class StudentController extends Controller
     {
         $this->view('students.edit');    
     }
+
+    public function store()
+    {
+        $studentModel = new Student();
+        $studentModel->insert($_POST);
+    }
 }
